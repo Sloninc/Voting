597741c5142dfa4b73f286d386ddb54d4d869528
Andrei Ruk <sloninc@mail.ru>
2023-11-13 18:38:47 +0300

    Написан метод InputData

--- Program.cs
+++ Program.cs
@@ -1,10 +1,63 @@
-﻿namespace Voting
+﻿using System;
+namespace Voting
 {
-    internal class Program
+    enum InputDataText
     {
-        static void Main(string[] args)
+        Blocks,
+        Candidates
+    }
+    public class Program
+    {
+        static int blocks;
+        static int candidateCount;
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            InputData(InputDataText.Blocks);
+            InputData(InputDataText.Candidates);
+            Console.ReadLine();
+        }
+        static void InputData(InputDataText inputData)
+        {
+            var isBlocks = inputData == InputDataText.Blocks ? true : false;
+            var blocksText = "Введите количество тестовых блоков: ";
+            var candodateText = "Введите количество кандидатов от 1 до 20: ";
+            var inputText = inputData== InputDataText.Blocks ? blocksText: candodateText;
+            Console.WriteLine(inputText);
+            Console.CursorTop = 0;
+            while (true)
+            {
+                Console.CursorLeft = inputText.Length;
+                var q = int.TryParse(Console.ReadLine(), out var inputValue);
+                if (q&&isBlocks&&inputValue>0)
+                {
+                    blocks = inputValue;
+                    Console.Clear();
+                    Console.WriteLine($"Вы ввели {blocks} тестовых блоков");
+                    Console.WriteLine("Нажмите любую клавишу для продолжения...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                }
+                else if(q && !isBlocks&& inputValue is (<=20 and >0))
+                {
+                    candidateCount = inputValue;
+                    Console.Clear();
+                    Console.WriteLine($"Вы ввели {candidateCount} кандидатов");
+                    Console.WriteLine("Нажмите любую клавишу для продолжения...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    if(isBlocks)
+                        Console.WriteLine("Количество тестовых блоков должно быть целым положительным числом\n" + inputText);
+                    else Console.WriteLine("Количество кандидатов должно быть целым положительным числом от 1 до 20\n" + inputText);
+                    Console.CursorTop = 1;
+                }
+            }
+            
         }
     }
 }
